85830df4418f221194ea40d911602d5455f453bc
Kausuru <kaushika_mki_e23@engug.ruh.ac.lk>
2024-12-11 12:19:00 +0530

    Deleted  readme And updated Client

---
+++ client/screens/auth/Register.js
@@ -0,0 +1,12 @@
+import {view, Text } from 'react-native';
+import React from 'react';
+
+const Register = () => {
+    return (
+        <view>
+            <Text>Register</Text>
+        </view>
+    );
+};
+
+export default Register;
